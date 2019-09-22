0ab9ed8645346a73c529b3d60ecc93557d0fa50d
Ricardo Manenti <rmanenti@gmail.com>
2019-09-22 18:29:56 -0300

    Added events to the slider.

--- js/modules/slide.js
+++ js/modules/slide.js
@@ -6,6 +6,13 @@ export default class Slide {
         
         this.wrapper = document.querySelector( configuration.getSelector( 'component', wrapper ) );
         this.list    = this.wrapper.querySelector( configuration.getSelector( 'component', list ) );
+        console.log( this.wrapper, this.list )
+
+        this.data = {
+            start     : 0,
+            current   : 0,
+            end : 0
+        }
     }
 
     initialize() {
@@ -13,6 +20,7 @@ export default class Slide {
         this.bindings();
 
         if ( this.wrapper !== undefined ) {
+
             this.wrapper.addEventListener( 'mousedown', this.start );
             this.wrapper.addEventListener( 'mouseup', this.stop );
         }
@@ -28,14 +36,34 @@ export default class Slide {
     start( event ) {
 
         event.preventDefault();
+
+        this.data.start = event.clientX;
         this.wrapper.addEventListener( 'mousemove', this.move );
     }
 
     move( event ) {
 
+        this.place( 
+            this.update( event.clientX ) );
+    }
+
+    place( position ) {
+        
+        this.data.current = position;
+        this.list.style.transform = `translate3d(${position}px, 0, 0)`;
+    }
+
+    update( position ) {
+        this.data.current = ( this.data.start - position ) * 1.6;
+        return ( this.data.end - this.data.current );
     }
 
     stop( event ) {
+
+        
         this.wrapper.removeEventListener( 'mousemove', this.move );
+        this.data.end = this.data.current;
+
+        console.log( this.data );
     }
 }
--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import configuration from './modules/configuration.js';
 import Slide         from './modules/slide.js';
 
-const slide = new Slide( 'slide' );
+const slide = new Slide( 'slide', 'slide-list' );
 slide.initialize();
